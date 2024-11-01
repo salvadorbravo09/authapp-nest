979b74ce3c722a4814a24a7576ce230e52fd1e69
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-11-01 17:40:56 -0300

    add cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Activar el CORS
+  app.enableCors();
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
